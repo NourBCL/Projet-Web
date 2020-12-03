ff4cbbd91dd7e602c4eee57b2f260ddf46a5ae69
unknown <User@PRT-HICHEMBCL.sndp.intra>
2020-12-03 14:29:58 +0100

    nour:first push

---
+++ controller/eventC.php
@@ -0,0 +1,89 @@
+<?PHP
+	include "./config.php";
+	require_once './Model/event.php';
+
+	class eventC {
+		
+		function ajouterEvn($event){
+			$sql="INSERT INTO event (id , nom, date, lieu, description, categorie, image  ) 
+			VALUES (:id,:nom,:date, :lieu , :description , :categorie , :image)";
+			$db = config::getConnexion();
+			try{
+				$query = $db->prepare($sql);
+			
+				$query->execute([
+					'id' => $event->getIdEvent(),
+					'nom' => $event->getNomEvent(),
+					'date' => $event->getDateEvent(),
+					'lieu' => $event->getLieuEvent(),
+					'description' => $event->getDescriptionEvent(),
+					'categorie' => $event->getCategorieEvent(),
+					'image' => $event->getImageEvent(),
+					
+				]);			
+			}
+			catch (Exception $e){
+				echo 'Erreur: '.$e->getMessage();
+			}			
+		}
+		
+		function afficherEvn(){
+			
+			$sql="SELECT * FROM event";
+			$db = config::getConnexion();
+			try{
+				$liste = $db->query($sql);
+				return $liste;
+			}
+			catch (Exception $e){
+				die('Erreur: '.$e->getMessage());
+			}	
+		}
+
+		function supprimerEvn($id){
+			$sql="DELETE FROM event WHERE id= :id";
+			$db = config::getConnexion();
+			$req=$db->prepare($sql);
+			$req->bindValue(':id',$id);
+			try{
+				$req->execute();
+			}
+			catch (Exception $e){
+				die('Erreur: '.$e->getMessage());
+			}
+		}
+		function modifierEvn($event, $id){
+			try {
+				$db = config::getConnexion();
+				$query = $db->prepare(
+					'UPDATE event SET 
+						id = :id, 
+						nom = :nom,
+						date = :date,
+						lieu = :lieu,
+						description = :description,
+						categorie = :categorie, 
+						image = :image
+						
+					WHERE id = :id'
+				);
+				$query->execute([
+					'id' => $event->getIdEvent(),
+					'nom' => $event->getNomEvent(),
+					'date' => $event->getDateEvent(),
+					'lieu' => $event->getLieuEvent(),
+					'description' => $event->getDescriptionEvent(),
+					'categorie' => $event->getCategorieEvent(),
+					'image' => $event->getImageEvent(),
+					'id' => $id
+				]);
+				echo $query->rowCount() . " records UPDATED successfully <br>";
+			} catch (PDOException $e) {
+				$e->getMessage();
+			}
+		}
+
+		
+	}
+
+?>
